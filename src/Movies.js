df7037561928fbc927f3df573f1eb9723de5dce2
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-06 15:54:46 -0600

    Movies component should be functional

--- src/Movies.js
+++ src/Movies.js
@@ -2,9 +2,23 @@ const { Component } = require("react")
 
 import React {Component} from 'react'
 
+function Movies (){
+  
+}
 class Movies extends Component{
   constructor(){
     super();
-    
+    this.state = {
+      
+    }
   }
+  findMovies(){
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+    .then(movies =>{
+      this.state.movies = movies.movies
+    })
+  }
+
 }
+export default Movies
