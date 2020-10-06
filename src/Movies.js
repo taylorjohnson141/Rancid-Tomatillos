ba8507f678066f26c296827c6a3a3f7fba490751
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-06 16:08:04 -0600

    Add async function to Movies

--- src/Movies.js
+++ src/Movies.js
@@ -1,24 +1,35 @@
+import React from 'react';
+
 const { Component } = require("react")
 
-import React {Component} from 'react'
+
+async function letfindMovies(){
+  let promise = fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+  .then(response => response.json())
+  .then(movies =>{
+    console.log(movies.movies)
+     return movies.movies
+  })
+  let result = await promise
+  return result
+}
 
 function Movies (){
-  
+  return(
+    <section>
+      <h1>{letfindMovies()}</h1>
+    </section>
+  )
 }
-class Movies extends Component{
-  constructor(){
-    super();
-    this.state = {
-      
-    }
-  }
-  findMovies(){
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
-    .then(movies =>{
-      this.state.movies = movies.movies
-    })
-  }
 
+render() {
+  return (
+    <div>
+      <ul className="list-group list-group-flush">
+        {this.state.Posts}
+      </ul>
+    </div>
+  );
 }
+
 export default Movies
