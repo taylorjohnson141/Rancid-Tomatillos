5c068272d1cf98d3003c63184689f54b34d60e0d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-07 15:39:49 -0600

    Import Login, assign to onClick for login-button

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Login from './Login.js'
 
 function Header() {
   return (
     <header className='App-header'>
       <section className='user-in-out-placeholder'>
-        <button className='login-button'>Login</button>
-        <button className='logout-button'>Logout</button>
+        <button className='login-button' onClick=<Login/>>Login</button>
+        // <button className='logout-button'>Logout</button>
       </section>
       <section className='genre-dropdown'>
         <button className='genre-dropdown'>Select Genre
