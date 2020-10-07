c97a2e4d08cd23959f3a4ca078a7a7f1db7216d1
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-07 15:25:50 -0600

    Add login component constructor

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const { Component } = require("react")
+
+class Login extends Component{
+  constructor(){
+    super()
+    this.state = {
+      response:[],
+      error :''
+    }
+  }
+  render() {
+    return (
+      <
+    )
+  }
+
+}
