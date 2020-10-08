0febbd1637f886ff1baf0d043716a775e0f73ce1
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-07 18:08:20 -0600

    No changes

--- src/Header.js
+++ src/Header.js
@@ -23,7 +23,6 @@ class Header extends Component{
     }
     return (
       <>
-      {this.state.displayLogin && <Login/>}
       <header className='App-header'>
         <section className='user-in-out-placeholder'>
       <button className='login-button' onClick= {this.displayLogin}>Login</button>
