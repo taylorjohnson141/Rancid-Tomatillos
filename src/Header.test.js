42a1151e4d702364475c715681f0026179f0da70
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-10 17:18:06 -0600

    Begin development of Header.test

---
+++ src/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Header from './Header';
+
+describe('Header', () => {
+  it('should render a header', () => {
+    Header.render();
+    //SETUP
+      // Render the Header with correct props passed in
+    })
+})
