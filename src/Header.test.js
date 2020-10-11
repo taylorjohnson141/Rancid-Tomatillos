30c5be7bd3d543b34feb1f0b31126d09f8e2c9fc
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-11 17:27:45 -0600

    Sad test for logout button not being in header

--- src/Header.test.js
+++ src/Header.test.js
@@ -7,26 +7,43 @@ import Header from './Header';
 describe('Header', () => {
   it('should render a header with login button when user is not logged in', () => {
 
+    //SETUP
+      // Render the Header with correct props passed in
     render(<Header
       userLoggedIn={false}
       changeLogin={() => {}}
       clickLogout={() => {}}
       />
     )
-    //SETUP
-      // Render the Header with correct props passed in
+
     expect(screen.getByText('Login')).toBeInTheDocument();
   })
 
   it('should render a header with logout button when user is logged in', () => {
+
+    //SETUP
+      // Render the Header with correct props passed in
     render(<Header
       userLoggedIn={true}
       changeLogin={() => {}}
       clickLogout={() => {}}
       />
     )
+
+    expect(screen.getByText('Logout')).toBeInTheDocument();
+  })
+
+  it('should render a header without logout button when user is not logged in', () => {
+
     //SETUP
       // Render the Header with correct props passed in
-    expect(screen.getByText('Logout')).toBeInTheDocument();
+    render(<Header
+      userLoggedIn={false}
+      changeLogin={() => {}}
+      clickLogout={() => {}}
+      />
+    )
+
+    expect(screen.queryByText('Logout')).toBeNull();
   })
 })
