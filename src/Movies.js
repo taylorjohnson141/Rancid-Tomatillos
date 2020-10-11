bf095f62ae0bd2a49b81e4516b931786127baa49
Taylor Johnson <taylorjohnson141@gmail.com>
2020-10-11 14:41:08 -0600

    Remove unnessasry code

--- src/Movies.js
+++ src/Movies.js
@@ -16,30 +16,13 @@ class Movies extends Component{
       let movies = response.movies
       this.setState({movies:movies})
     }
-    findMovie(){
-      fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.id}`)
-      .then(movie => movie.json())
-      .then(movieResponse => {
-        this.setState({movie:movieResponse})
-        let {movie} = this.state
-        return(<section>
-          <img src = {movie.backdrop_path}></img>
-          <article>
-          <h2 className = 'movie-title'>{movie.title}</h2>
-          <h2 className = 'movie-tagline'>{movie.tagline}</h2>
-            <div>Average Rating:{movie.average_rating}</div>
-            <div>Release Date:{movie.release_date} </div>
-      <p>{movie.overview}</p>
-          </article>
-          </section>
-        )
-    })
-    }
+
     findMovie = (event) =>{
       let id =event.target.closest('section').id
       this.setState({clicked:true})
       this.setState({id:id})
     }
+    
   getMovies(){
     return this.state.movies.map(movie => {
       return( <section className = 'movie-section' id ={movie.id} onClick = {this.findMovie}>
