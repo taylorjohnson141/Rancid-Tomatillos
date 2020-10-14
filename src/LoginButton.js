2be5755ada4d152ca87f254423431e7561c230ef
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-14 16:54:09 -0600

    Create LoginButton component

---
+++ src/LoginButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+import { Router, NavLink } from 'react-router-dom';
+
+class LoginButton extends Component{
+  constructor(props) {
+    super(props)
+      this.state = {}
+    }
+  render() {
+    return (
+        <NavLink to='/login' className='login-button'>Login</NavLink>
+    )
+  }
+}
+
+export default LoginButton;
