12d8e371cb972b49c6d0adab3513e43004439b6c
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-16 20:37:19 -0600

    Intitialize creation of Movie.test.js

---
+++ src/Movie.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Movie from './Movie';
+
+describe('Movie', () => {
+  it('should render movie details', () => {
+
+    render(<Movie
+
+      />
+    )
+
+
+  })
+
+
+})
