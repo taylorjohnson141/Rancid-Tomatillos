e41589f1bb3974049610813c43177e258e4f7b43
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-10-16 20:34:21 -0600

    Refactor api calling function into Movie.js through apiCalls.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getSingleMovieInfo } from './apiCalls';
 
 class Movie extends Component {
   constructor(prop){
@@ -22,21 +23,11 @@ class Movie extends Component {
     </section>
     )
   }
-  // async componentDidUpdate(){
-  //   console.log('hello')
-  //   const match = this.props.match
-  //   console.log(match)
-  //   let movie = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${match}`)
-  //   let response = await movie.json()
-  //   this.setState({movie:response.movie})
-  // }
+
   async componentDidMount(){
-    console.log('hello')
     const match = this.props.match
-    console.log(match)
-    let movie = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${match}`)
-    let response = await movie.json()
-    this.setState({movie:response.movie})
+    let movie = await getSingleMovieInfo(match)
+      this.setState({movie:movie.movie})
   }
 
 
